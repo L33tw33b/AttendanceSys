1f96d5193c3fb4df64a6f43327b727bd520d898b
L33tw33b <ronaldyungkuen@gmail.com>
2022-04-30 13:44:26 +0800

    Add project files.

---
+++ AttendanceSys/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace AttendanceSys
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt_input = new System.Windows.Forms.TextBox();
+            this.lbl_welcome = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pbox_slide = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox_slide)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_input
+            // 
+            this.txt_input.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_input.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_input.Location = new System.Drawing.Point(646, 726);
+            this.txt_input.Name = "txt_input";
+            this.txt_input.PasswordChar = '*';
+            this.txt_input.Size = new System.Drawing.Size(622, 62);
+            this.txt_input.TabIndex = 0;
+            this.txt_input.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_input_KeyDown);
+            // 
+            // lbl_welcome
+            // 
+            this.lbl_welcome.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_welcome.AutoSize = true;
+            this.lbl_welcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_welcome.Location = new System.Drawing.Point(636, 668);
+            this.lbl_welcome.Name = "lbl_welcome";
+            this.lbl_welcome.Size = new System.Drawing.Size(278, 55);
+            this.lbl_welcome.TabIndex = 1;
+            this.lbl_welcome.Text = "Placeholder";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(381, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(656, 367);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pbox_slide
+            // 
+            this.pbox_slide.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.pbox_slide.Location = new System.Drawing.Point(30, 334);
+            this.pbox_slide.Name = "pbox_slide";
+            this.pbox_slide.Size = new System.Drawing.Size(531, 454);
+            this.pbox_slide.TabIndex = 3;
+            this.pbox_slide.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 800);
+            this.Controls.Add(this.pbox_slide);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbl_welcome);
+            this.Controls.Add(this.txt_input);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox_slide)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_input;
+        private System.Windows.Forms.Label lbl_welcome;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pbox_slide;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ AttendanceSys/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace AttendanceSys
+{
+
+    public partial class Form1 : Form
+    {
+
+        // Excel
+        _Application excelApp = new _Excel.Application();
+        Workbook wb;
+        Worksheet ws;
+
+        // App.config
+        static readonly int namepos = Properties.Settings.Default.Namepos;
+        static readonly int cardpos = Properties.Settings.Default.Cardnopos;
+        static readonly int telepos = Properties.Settings.Default.Telepos;
+        static readonly string textdir = ConfigurationManager.AppSettings["textpath"];
+        static readonly string exceldir = ConfigurationManager.AppSettings["excelpath"];
+        string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name.Split('\\')[1];
+
+        // File
+        string filen = $@"{textdir}{DateTime.UtcNow.Date.ToString("dd_MM_yyyy")}.txt";
+        string[] pics = { "pics/1.jpg", "pics/2.jpg", "pics/3.jpg" };
+
+        // Misc
+        int i = 0;
+        string[] name = new string[2000];
+        string[] tele = new string[2000];
+        string[] card = new string[2000];
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void nextImage() {
+            if (i == 2)
+            {
+                i = 0;
+            }
+            else
+            {
+                i++;
+            }
+
+            pbox_slide.Image = Image.FromFile(pics[i]);
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) // Load
+        {
+            txt_input.Focus();
+            lbl_welcome.Text = "";
+            pbox_slide.ImageLocation = string.Format(@"pics/1.jpg");
+            if (!File.Exists(filen)) {
+                File.Create(filen);
+            }
+
+            if (excelApp == null) {
+                MessageBox.Show("Excel is not installed!", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            wb = excelApp.Workbooks.Open($@"{exceldir}");
+            ws = wb.ActiveSheet;
+            for (int i = 0; i < ws.UsedRange.Rows.Count; i++) {
+                name[i] = Convert.ToString(ws.Cells[i + 1, namepos].Value);
+            }
+            for (int i = 0; i < ws.UsedRange.Rows.Count  ; i++)
+            {
+                tele[i] = Convert.ToString(ws.Cells[i + 1, telepos].Value);
+            }
+            for (int i = 0; i < ws.UsedRange.Rows.Count ; i++)
+            {
+                card[i] = Convert.ToString(ws.Cells[i + 1, cardpos].Value);
+            }
+            wb.Close(0);
+            excelApp.Quit();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) //Timer 
+        {
+            Image img = pbox_slide.Image;
+            nextImage();
+            img.Dispose();
+        }
+
+        private void txt_input_KeyDown(object sender, KeyEventArgs e) // Enter
+        {
+            if (e.KeyCode == Keys.Enter) {
+                bool found = false;
+                int i = 0;
+                if (txt_input.TextLength == 8) { // User input == Telephone number
+                    for (;i < tele.Length;i++) {
+                        if (txt_input.Text == tele[i]) { // Excel Cells[y,x]
+                            found = true;
+                            using (StreamWriter sw = File.AppendText($@"{textdir}{DateTime.UtcNow.Date.ToString("dd_MM_yyyy")}.txt")) {
+                                sw.WriteLine(/*ws.Cells[i, namepos].Value.ToString()*/string.Format("{0,-25}{1,-30}", name[i], DateTime.Now.ToString())) ;
+                            }
+                            
+                                break;
+                        }
+                    }
+                }
+                else if (txt_input.TextLength == 10) { // User input == Card ID
+                    for (; i < card.Length; i++)
+                    {
+                        if (txt_input.Text == card[i])
+                        {
+                            found = true;
+                            using (StreamWriter sw = File.AppendText($@"{textdir}{DateTime.UtcNow.Date.ToString("dd_MM_yyyy")}.txt"))
+                            {
+                                try {
+                                    sw.WriteLine(/*ws.Cells[i, namepos].Value.ToString()*/ string.Format("{0,-25}{1,-30}", name[i], DateTime.Now.ToString()));
+                                }catch(Exception ex)
+                                {
+                                    MessageBox.Show(ex.Message);
+                                }
+                                
+                            }
+                            break;
+                        }
+                    }
+                }
+                
+                if (!found) { MessageBox.Show("Invalid ID!", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning); }
+                else
+                {
+                    lbl_welcome.Text = $"歡迎你!{/*ws.Cells[i, namepos].Value.ToString()*/name[i]}";
+                }
+
+                txt_input.Text = "";
+            }
+        }
+
+
+    }
+}
